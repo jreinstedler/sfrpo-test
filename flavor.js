6ba727d7823e4797a3d65253b3c96b9ab2994593
Jason <2791186+jreinstedler@users.noreply.github.com>
2020-04-29 10:42:22 -0700

    Create flavor.js

---
+++ flavor.js
@@ -0,0 +1,16 @@
+ooh secrets and stuff
+
+include "com.rally.cognito"
+include "com.rally.taki"
+include "rally-io-rnbcomm-service"
+include "rally-shoutout-admin"
+include "rallynetworkio"
+
+metadata:
+  annotations:
+    kompose.cmd: kompose convert
+    kompose.version: 1.21.0 (992df58d8)
+  creationTimestamp: null
+  labels:
+    io.kompose.service: rally-io-rnbcomm-service
+  name: rally-io-rnbcomm-service
